dc9fca014e2396feb901f2a9520bc178471543c8
Jason Liu <jliu@lcs.on.ca>
2019-04-05 09:20:59 -0400

    Working on background and create all the classes

--- MyGame/Reflexes.java
+++ MyGame/Reflexes.java
@@ -18,5 +18,7 @@ public class Reflexes extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1);
         
+        
+        
     }
 }
---
+++ MyGame/TrackingSquare.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TrackingSquare here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TrackingSquare extends Squares
+{
+    /**
+     * Act - do whatever the TrackingSquare wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
